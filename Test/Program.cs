6a425ee41a78071bdcef190bbb93c1c9568cc66c
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-08-04 13:29:49 +0400

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Model.LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            Console.WriteLine("Добавили 5 элементов.");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            list.Delete(3);
+            Console.WriteLine("\nУдалили 3 элемент.");
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            list.AppendHead(10);
+            Console.WriteLine("\nДобавили 10 элемент в начало.");
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nДобавили 20 элемент после 4.");
+            list.InsertAfter(4, 20);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
