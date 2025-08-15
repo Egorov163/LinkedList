3b51c54c49090650018758f59987ff5e904665a2
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-08-15 16:34:44 +0400

    Очередь, Дек (Queue, Deque)

---
+++ Test/Model/DuplexLinkedDeque/DuplexLinkedDeque.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace Test.Model.DuplexLinkedDeque
+{
+    public class DuplexLinkedDeque<T>
+    {
+        private DuplexItem<T> head;
+        private DuplexItem<T> tail;
+        public int Count { get; private set; }
+
+        public DuplexLinkedDeque() { }
+        public DuplexLinkedDeque(T data)
+        {
+            SetHeadItem(data);
+        }
+
+        private void SetHeadItem(T data)
+        {
+            var item = new DuplexItem<T>(data);
+            head = item;
+            tail = item;
+            Count = 1;
+        }
+
+        public void PushBack(T data)
+        {
+            if (Count == 0)
+            {
+                SetHeadItem(data);
+                return;
+            }
+
+            var item = new DuplexItem<T>(data);
+            item.Next = tail;
+            tail.Previous = item;
+            tail = item;
+            Count++;
+        }
+
+        public void PushFront(T data)
+        {
+            if (Count == 0)
+            {
+                SetHeadItem(data);
+                return;
+            }
+
+            var item = new DuplexItem<T>(data);
+            item.Previous = head;
+            head.Next = item;
+            head = item;
+            Count++;
+        }
+
+        public T PopBack()
+        {
+            if (Count > 0)
+            {
+                var result = tail.Data;
+                var current = tail.Next;
+                current.Previous = null;
+                tail = current;
+                Count--;
+                return result;
+            }
+            else
+            {
+                return default; 
+            }
+        }
+
+        public T PopFront()
+        {
+            if (Count > 0)
+            {
+                var result = head.Data;
+                var current = head.Previous;
+                current.Next = null;
+                head = current;
+                Count--;
+                return result;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public T PeekBack()
+        {
+            return tail.Data;
+        }
+
+        public T PeekFront()
+        {
+            return head.Data;
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Test.Model;
+using Test.Model.DuplexLinkedDeque;
 
 namespace Test
 {
@@ -6,7 +8,76 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            #region DuplexLinkedDeque
+            var duplexLinkedDeque = new DuplexLinkedDeque<int>();
+            duplexLinkedDeque.PushFront(1);
+            duplexLinkedDeque.PushFront(2);
+            duplexLinkedDeque.PushFront(3);
+            duplexLinkedDeque.PushBack(4);
+            duplexLinkedDeque.PushBack(5);
 
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+
+
+            #endregion
+
+            #region EasyDeque
+            //var easyDeque = new EasyDeque<int>();
+            //easyDeque.PushFront(1);
+            //easyDeque.PushFront(2);
+            //easyDeque.PushFront(3);
+            //easyDeque.PushBack(4);
+            //easyDeque.PushBack(5);
+
+            //Console.WriteLine(easyDeque.PopFront());
+            //Console.WriteLine(easyDeque.PopBack());
+            //Console.WriteLine(easyDeque.PopFront());
+            //Console.WriteLine(easyDeque.PopBack());
+            #endregion
+
+            #region LinkedQueue
+            //var linkedQueue = new LinkedQueue<int>();
+            //linkedQueue.Enqueue(1);
+            //linkedQueue.Enqueue(2);
+            //linkedQueue.Enqueue(3);
+            //linkedQueue.Enqueue(4);
+            //linkedQueue.Enqueue(5);
+
+            //Console.WriteLine(linkedQueue.Dequeue());
+            //Console.WriteLine(linkedQueue.Peek());
+            //Console.WriteLine(linkedQueue.Dequeue());
+            //Console.WriteLine(linkedQueue.Dequeue());
+            //Console.WriteLine(linkedQueue.Dequeue());
+            #endregion
+
+            //#region ArrayQueue
+            //var arrayQueue = new ArrayQueue<int>(10);
+            //arrayQueue.Enqueue(1);
+            //arrayQueue.Enqueue(2);
+            //arrayQueue.Enqueue(3);
+            //arrayQueue.Enqueue(4);
+            //arrayQueue.Enqueue(5);
+
+            //Console.WriteLine(arrayQueue.Dequeue());
+            //Console.WriteLine(arrayQueue.Peek());
+            //Console.WriteLine(arrayQueue.Dequeue());
+            //#endregion
+
+            #region EasyQueue
+            //var easyQueue = new EasyQueue<int>();
+            //easyQueue.Enqueue(1);
+            //easyQueue.Enqueue(2);
+            //easyQueue.Enqueue(3);
+            //easyQueue.Enqueue(4);
+            //easyQueue.Enqueue(5);
+
+            //Console.WriteLine(easyQueue.Dequeue());
+            //Console.WriteLine(easyQueue.Peek());
+            //Console.WriteLine(easyQueue.Dequeue());
+            #endregion
         }
     }
 }
