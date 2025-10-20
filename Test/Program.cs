17d2f666128fd38808442b21d26dfd2813143c35
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-10-20 13:40:48 +0400

    Пример реализации Бинарное дерево.

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,35 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var tree = new Tree<int>();
 
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+
+            Console.WriteLine("Префиксный обход");
+            foreach (var item in tree.Preorder())
+            {
+                Console.WriteLine(item + ", ");
+            }
+
+            Console.WriteLine("\nПостфиксный обход");
+            foreach (var item in tree.Postorder())
+            {
+                Console.WriteLine(item + ", ");
+            }
+
+            Console.WriteLine("\nИнфиксный обход");
+            foreach (var item in tree.Inorder())
+            {
+                Console.WriteLine(item + ", ");
+            }
         }
     }
 }
