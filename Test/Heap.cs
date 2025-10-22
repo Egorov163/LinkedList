1fd1037665bd7a43400fb3e196649cb133719980
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-10-22 11:53:25 +0400

    реализовал двоичную кучу

---
+++ Test/Heap.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Test
+{
+    public class Heap : IEnumerable
+    {
+        private List<int> items = new List<int>();
+        public int Count => items.Count;
+
+        public int? Peek()
+        {
+            if (Count > 0)
+            {
+                return items[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Heap() { }
+        public Heap(List<int> items)
+        {
+            this.items.AddRange(items);
+            for (int i = Count; i >= 0; i--)
+            {
+                Sort(i);
+            }
+        }
+
+        public void Add(int item)
+        {
+            items.Add(item);
+
+            var currentIndex = Count - 1;
+            var parrentIndex = GetParrentIndex(currentIndex);
+
+            while (currentIndex > 0 && items[parrentIndex] < items[currentIndex])
+            {
+                Swap(currentIndex, parrentIndex);
+
+                currentIndex = parrentIndex;
+                parrentIndex = GetParrentIndex(currentIndex);
+            }
+        }
+
+        public int GetMax()
+        {
+            var result = items[0];
+            items[0] = items[Count - 1];
+            items.RemoveAt(Count - 1);
+            Sort(0);
+            return result;
+        }
+
+        private void Sort(int currentIndex)
+        {
+            int maxIndex = currentIndex;
+            int leftIndex;
+            int rightIndex;
+
+            while (currentIndex < Count)
+            {
+                leftIndex = 2 * currentIndex + 1;
+                rightIndex = 2 * currentIndex + 2;
+
+                if (leftIndex < Count && items[leftIndex] > items[maxIndex])
+                {
+                    maxIndex = leftIndex;
+                }
+
+                if (rightIndex < Count && items[rightIndex] > items[maxIndex])
+                {
+                    maxIndex = rightIndex;
+                }
+
+                if (maxIndex == currentIndex)
+                {
+                    break;
+                }
+
+                Swap(currentIndex, maxIndex);
+                currentIndex = maxIndex;
+            }
+        }
+
+        private void Swap(int currentIndex, int parrentIndex)
+        {
+            var temp = items[currentIndex];
+            items[currentIndex] = items[parrentIndex];
+            items[parrentIndex] = temp;
+        }
+
+        private static int GetParrentIndex(int currentIndex)
+        {
+            return (currentIndex - 1) / 2;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (Count > 0)
+            {
+                yield return GetMax();
+            }
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Test
 {
@@ -6,7 +8,35 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var timer = new Stopwatch();
 
+            var rnd = new Random();
+            var startItems = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                startItems.Add(rnd.Next(-1000, 1000));
+            }
+
+            timer.Start();
+            var heap = new Heap(startItems);
+            timer.Stop();
+            Console.WriteLine("Первоначальная инициализация" + timer.Elapsed);
+
+            timer.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                heap.Add(rnd.Next(-1000, 1000));
+            }
+            timer.Stop();
+            Console.WriteLine("Второе добавление элементов" + timer.Elapsed);
+
+            timer.Restart();
+            foreach (var item in heap)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Вывод элементов" + timer.Elapsed);
         }
     }
 }
