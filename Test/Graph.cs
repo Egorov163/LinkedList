dffc234f5e644874fcdd866a45f981ae72ccc8c7
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-10-23 11:11:53 +0400

    Реализовал пример Графа.

---
+++ Test/Graph.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace Test
+{
+    public class Graph
+    {
+        List<Vertex> Vertices = new List<Vertex>();
+        List<Edge> Edges = new List<Edge>();
+        public int VertexCount => Vertices.Count;
+        public int EdgeCount => Edges.Count;
+
+        public void AddVertex(Vertex vertex)
+        {
+            Vertices.Add(vertex);
+        }
+
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            Edges.Add(edge);
+        }
+
+        public int[,] GetMatrix()
+        {
+            var matrix = new int[Vertices.Count, Vertices.Count];
+
+            foreach (var edge in Edges)
+            {
+                var row = edge.From.Number - 1;
+                var column = edge.To.Number - 1;
+
+                matrix[row, column] = edge.Weight;
+            }
+
+            return matrix;
+        }
+
+        public List<Vertex> GetVertexList(Vertex vertex)
+        {
+            var result = new List<Vertex>();
+
+            foreach (var edge in Edges)
+            {
+                if (edge.From == vertex)
+                {
+                    result.Add(edge.To);
+                }
+            }
+
+            return result;
+        }
+        public bool Wave(Vertex start, Vertex finish)
+        {
+            var result = new List<Vertex>();
+
+            var list = new List<Vertex>();
+
+            list.Add(start);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var vertex = list[i];
+
+                foreach (var v in GetVertexList(vertex))
+                {
+                    if (!list.Contains(v))
+                    {
+                        list.Add(v);
+                    }
+                }
+            }
+
+            return list.Contains(finish);
+        }
+    }
+}
---
+++ Test/Vertex.cs
@@ -0,0 +1,17 @@
+﻿namespace Test
+{
+    public class Vertex
+    {
+        public int Number { get; set; }
+
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+}
